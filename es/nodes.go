49fc4b4a179d0d712093423fbd1d5b9f969e1403
Oleksiy Voronin <ovoronin@gmail.com>
2018-02-13 21:07:29 -0500

    Forgotten file

--- es/nodes.go
+++ es/nodes.go
@@ -1,13 +1,9 @@
 package es
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"strconv"
 	"strings"
-
-	"github.com/fatih/color"
 )
 
 // NodeJvmStats contains JVM statistics for node
@@ -101,112 +97,4 @@ func (e Es) GetNodeStats(nodes []string) (*NodesStats, error) {
 		return nil, err
 	}
 	return stats, nil
-}
-
-func (n NodesStats) String(dc func(format string, a ...interface{}) string) string {
-	var buffer bytes.Buffer
-	undr := color.New(color.Underline).SprintfFunc()
-	for nodeID := range n.Nodes {
-		buffer.WriteString("\n")
-		buffer.WriteString(dc(undr(nodeID)))
-		buffer.WriteString(dc(":"))
-		buffer.WriteString(dc("%v", n.Nodes[nodeID]))
-	}
-
-	return buffer.String()
-}
-
-func (n NodeStats) String() string {
-	return pad(fmt.Sprintf("\n"+
-		"Name: %s\n"+
-		"Transport Address: %s\n"+
-		"Host: %s\n"+
-		"JVM: %v\n"+
-		"Indices: %v\n"+
-		"Filesystem: %v",
-		n.Name, n.TransportAddress, n.Host, n.JVM, n.Indices, n.FS), 2)
-}
-
-func (i NodeIndicesStats) String() string {
-	return pad(fmt.Sprintf(
-		"\nDocuments:\n"+
-			"  Count: %d\n"+
-			"  Deleted: %d\n"+
-			"Store:\n"+
-			"  Size: %.1f Mb\n"+
-			"  Throttle Time: %d sec",
-		i.Docs.Count, i.Docs.Deleted, megabytes(i.Store.Size), i.Store.ThrottleTime/1000), 2)
-}
-
-func (j NodeJvmStats) String() string {
-	return pad(fmt.Sprintf(
-		"\nUptime: %s"+
-			"\nMemory: %v"+
-			"\nThreads: %v",
-		fmtTime(j.Uptime/1000), j.Memory, j.Threads), 2)
-}
-
-func (t NodeThreadStats) String() string {
-	return pad(fmt.Sprintf(
-		"\nCount: %d"+
-			"\nPeak Count: %d",
-		t.Count, t.PeakCount), 2)
-}
-
-func (m NodeMemoryStats) String() string {
-	return pad(fmt.Sprintf(
-		"\n"+
-			"Heap Used: %.1f Mb\n"+
-			"Heap Committed: %.1f Mb\n"+
-			"Heap Max: %.1f Mb\n"+
-			"Non-Heap Used: %.1f Mb\n"+
-			"Non-Heap Committed: %.1f Mb",
-		megabytes(m.HeapUsed), megabytes(m.HeapCommitted), megabytes(m.HeapMax),
-		megabytes(m.NonHeapUsed), megabytes(m.NonHeapCommitted)), 2)
-}
-
-func (f FsStats) String() string {
-	var buffer bytes.Buffer
-	for i, n := range f.Data {
-		buffer.WriteString("\n    " + strconv.FormatInt(int64(i), 10) + ":")
-		buffer.WriteString(pad(n.String(), 4))
-		buffer.WriteString("\n")
-	}
-	data := buffer.String()
-	return pad(fmt.Sprintf(
-		"\nTotal:\n"+
-			"  Total: %6.1f Mb\n"+
-			"  Free: %6.1f Mb\n"+
-			"  Available: %6.1f Mb\n"+
-			"  Data: %v",
-		megabytes(f.Total.Total), megabytes(f.Total.Free), megabytes(f.Total.Available),
-		data), 2)
-}
-
-func (f FsNodeStats) String() string {
-	return pad(fmt.Sprintf(
-		"\n"+
-			"Path: %s\n"+
-			"Mount: %s\n"+
-			"FS Type: %s\n"+
-			"Total: %.1f Mb\n"+
-			"Free: %.1f Mb\n"+
-			"Available: %.1f Mb",
-		f.Path, f.Mount, f.Type, megabytes(f.Total), megabytes(f.Free), megabytes(f.Available)), 2)
-}
-
-func megabytes(b int64) float64 {
-	return float64(b) / 1024.0 / 1024.0
-}
-
-func fmtTime(seconds int64) string {
-	minutes := seconds / 60
-	seconds = seconds % 60
-	hours := minutes / 60
-	minutes = minutes % 60
-	return fmt.Sprintf("%02d:%02d:%02d", hours, minutes, seconds)
-}
-
-func pad(s string, num int) string {
-	return strings.Replace(s, "\n", "\n"+strings.Repeat(" ", num), -1)
 }
